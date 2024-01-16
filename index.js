0206cc9bfb8cdcf30f36df94eed5d7af6b98a4ef
David Lee <davidlee221@gmail.com>
2024-01-15 20:13:44 -0800

    adjusting darkmode

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // import axios from '/node_modules/axios';
 
-const button = document.getElementById("darkmode");
+const button = document.getElementById("dark-mode-text");
 const paragraph = document.querySelector("p");
 
 export function updateButton() {
-  if (button.value === "Start machine") {
-    button.value = "Stop machine";
+  if (button.innerText === "DARK") {
+    button.innerText = "LIGHT";
     paragraph.textContent = "The machine has started!";
   } else {
-    button.value = "Start machine";
+    button.value = "DARK";
     paragraph.textContent = "The machine is stopped.";
   }
 }
@@ -16,7 +16,7 @@ export function updateButton() {
 export function githubSearch(event) {
   var username = document.getElementById("username").value;
   // console.debug('what is formData', formData.entries())
-  axios.get('https://api.github.com/users/octocat')
+  fetch.get('https://api.github.com/users/octocat')
   .then(response => {
     // Handle the response data
     console.log(response.data);
